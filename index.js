0083b79b63efb6459b9d445c4fab006a5157e2e6
benjokapu <144476747+benjokapu@users.noreply.github.com>
2024-06-18 11:40:04 -0300

    Modificaciones

--- index.js
+++ index.js
@@ -7,37 +7,31 @@ deleteById(id): recibe el id de una pizza y la elimina en la base de datos
 
 */
 
-import mysql2 from "mysql2/promise"
-
-const connection = await mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'rootroot',
-    database: 'ejercitacion pizza'
-})
-
-async function getAll(){
-    return await connection.query("SELECT * from pizza;"),[0];
+async function getAll() {
+    const [rows] = await connection.query("SELECT * from pizza;");
+    return rows;
 }
 
-async function insert(pizza){
-    return await connection.query("INSERT INTO (nombre, libreGluten, importe, description) VALUES (?,?,?,?);", [pizza.nombre, pizza.libreGluten, pizza.importe, pizza.description])
+async function insert(pizza) {
+    return await connection.query("INSERT INTO pizza (nombre, libreGluten, importe, description) VALUES (?,?,?,?);", 
+    [pizza.nombre, pizza.libreGluten, pizza.importe, pizza.description]);
 }
 
-async function update(pizza){
-    if(!pizza.id){
+async function update(pizza) {
+    if (!pizza.id) {
         console.log("Se necesita el id para modificar una pizza");
         return;
     }
-    return await connection.query("UPDATE pizza SET nombre=?, libreGluten=?, importe=?, description=? WHERE id=?;", [pizza.nombre, pizza.libreGluten, pizza.importe, pizza.description, pizza.id])
+    return await connection.query("UPDATE pizza SET nombre=?, libreGluten=?, importe=?, description=? WHERE id=?;", 
+    [pizza.nombre, pizza.libreGluten, pizza.importe, pizza.description, pizza.id]);
 }
 
-async function deleteById(id){
-    if(!pizza.id){
+async function deleteById(id) {
+    if (!id) {
         console.log("Se necesita el id para eliminar una pizza");
         return;
     }
-    return await connection.query("DELETE FROM pizza WHERE id=?;",[pizza.id])
+    return await connection.query("DELETE FROM pizza WHERE id=?;", [id]);
 }
 
 insert({
