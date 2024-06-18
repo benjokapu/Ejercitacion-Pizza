b876e5afe0bcb4c9da67144bd9bd8e61f9287275
benjokapu <144476747+benjokapu@users.noreply.github.com>
2024-06-18 11:17:25 -0300

    Primera Parte Hecha

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /* 
+
 getAll(): devuelve todas las pizzas presentes en la tabla "Pizza" de la base de datos
 insert(pizza): recibe un objeto con todas las propiedades de una pizza (menos el id) y la inserta en la base de datos
 update(pizza): recibe un objeto con el id (obligatorio) y los campos que se quieren actualizar y modifica la pizza correspondiente en la base de datos
 deleteById(id): recibe el id de una pizza y la elimina en la base de datos 
+
 */
 
 import mysql2 from "mysql2/promise"
